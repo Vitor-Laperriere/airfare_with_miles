fd00045924ea163ab01e4537c9b75c2626027934
Frederico Baker <fredericodsbaker@gmail.com>
2024-11-26 21:21:15 -0300

    feat: limited date range selectable and added date widget

--- tickets_with_miles/flights/forms.py
+++ tickets_with_miles/flights/forms.py
@@ -1,14 +1,23 @@
 from django import forms
 from django.core.exceptions import ValidationError
-from datetime import date
+from datetime import date, timedelta
 
 class FlightSearchForm(forms.Form):
-    origin = forms.CharField(label='Origin Airport Code', max_length=3)
-    destination = forms.CharField(label='Destination Airport Code', max_length=3)
-    date = forms.DateField(label='Departure Date', widget=forms.SelectDateWidget)
+    origin = forms.CharField(label='Origem', max_length=3)
+    destination = forms.CharField(label='Destino', max_length=3)
+    date = forms.DateField(
+        label='Data',
+        widget=forms.DateInput(
+            attrs={
+                'type': 'date',
+                'min': date.today().strftime('%Y-%m-%d'),
+                'max': (date.today() + timedelta(days=329)).strftime('%Y-%m-%d')
+            }
+        )
+    )
 
     def clean_date(self):
         departure_date = self.cleaned_data['date']
         if departure_date < date.today():
-            raise ValidationError("Departure date cannot be in the past.")
+            raise ValidationError("A data não pode ser no passado.")
         return departure_date
--- tickets_with_miles/tickets_with_miles/settings.py
+++ tickets_with_miles/tickets_with_miles/settings.py
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tickets_with_miles.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
