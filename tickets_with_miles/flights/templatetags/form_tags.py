1c2104eabedf2e52673fa1d165d631812eed25e4
Daniel Barbosa <danielolibar@gmail.com>
2024-12-02 17:38:48 -0300

    feat: add option to insert flexibility

--- tickets_with_miles/flights/templatetags/form_tags.py
+++ tickets_with_miles/flights/templatetags/form_tags.py
@@ -1,7 +1,12 @@
 from django import template
+from datetime import datetime
 
 register = template.Library()
 
 @register.filter(name='add_class')
 def add_class(field, css_class):
     return field.as_widget(attrs={"class": css_class})
+
+@register.filter
+def to_datetime(value, format="%Y-%m-%dT%H:%M:%S"):
+    return datetime.strptime(value, format)
